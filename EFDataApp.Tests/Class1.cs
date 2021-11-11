c267156d69fd00d18123b2c2e8a986d4e66af5cb
Irina-myagkova <ira_myagkova@mail.ru>
2021-11-11 17:35:00 +0700

    Добавьте файлы проекта.

---
+++ EFDataApp.Tests/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+
+
+namespace EFDataApp.Tests
+{
+    [TestFixture]
+    public class ProjectTest
+    {
+        [Test]
+        public void Program_Test()
+        {
+            var webHost = Microsoft.AspNetCore.WebHost.CreateDefaultBuilder().UseStartup<Startup>().Build();
+            Assert.IsNotNull(webHost);
+        }
+
+
+    }
+}
